707a895093456828bb3f83202352ad9ee9141590
Douwe-Spaanderman <dspaanderman@gmail.com>
2023-10-26 16:57:00 +0200

    Changed resampling method, to hopefully speed up the process

--- interactivenet/utils/resample.py
+++ interactivenet/utils/resample.py
@@ -20,6 +20,7 @@ import torch
 from skimage.transform import resize
 from nibabel import affines
 import numpy.linalg as npl
+from PIL import Image
 
 from interactivenet.utils.utils import to_array
 
@@ -37,15 +38,13 @@ def resample_label(
         for class_ in range(1, int(n_class) + 1):
             for depth_ in range(depth):
                 mask = label[0, :, :, depth_] == class_
-                resized_2d = resize(
-                    mask.astype(float),
-                    shape_2d,
-                    order=1,
-                    mode="edge",
-                    cval=0,
-                    clip=True,
-                    anti_aliasing=False,
-                )
+                # Using PIL where possible for speedup
+                mask = Image.fromarray(mask)
+                resized_2d = mask.resize(shape_2d, resample=Image.BILINEAR)
+                resized_2d = np.asarray(resized_2d)
+                print("USING THE NEW PIL RESAMPLING -- THIS IS JUST FOR DEBUGGING")
+                # Copying is required for protected flag...
+                resized_2d = resized_2d.copy() 
                 reshaped_2d[:, :, depth_][resized_2d >= 0.5] = class_
         for class_ in range(1, int(n_class) + 1):
             mask = reshaped_2d == class_
@@ -86,16 +85,16 @@ def resample_image(
             resized_slices = []
             for i in range(image_c.shape[-1]):
                 image_c_2d_slice = image_c[:, :, i]
-                image_c_2d_slice = resize(
-                    image_c_2d_slice,
-                    shape[:-1],
-                    order=3,
-                    mode="edge",
-                    cval=0,
-                    clip=True,
-                    anti_aliasing=False,
-                )
+                print("USING THE NEW PIL RESAMPLING -- THIS IS JUST FOR DEBUGGING")
+                vmin, vmax = image_c_2d_slice.min(), image_c_2d_slice.max()
+                # Using PIL where possible for speedup
+                image_c_2d_slice = Image.fromarray(
+                    image_c_2d_slice
+                    ).resize(shape[:-1], resample=Image.BICUBIC)
+                # Using numpy clip as this was originally in resize
+                image_c_2d_slice = np.clip(image_c_2d_slice, vmin, vmax)
                 resized_slices.append(image_c_2d_slice)
+
             resized = np.stack(resized_slices, axis=-1)
             resized = resize(
                 resized,
